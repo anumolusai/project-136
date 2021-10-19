e5ec7d855601084c8e7a1dfdeaa7665bfff04c8f
anumolusai <92395360+anumolusai@users.noreply.github.com>
2021-10-19 21:40:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+status = "";
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    }
+    function draw() {
+    image(video, 0, 0, 480, 380);
+    }
+    function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting objects";
+    }
+    function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    }
